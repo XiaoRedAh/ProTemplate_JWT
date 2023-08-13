609f816ad5adbfe72fde30aec3ef37411c05c0a9
XiaoRedAh <2976698583@qq.com>
2023-08-13 16:36:21 +0800

    请求参数校验

--- backend/src/main/java/com/xiaoRed/controller/AuthorizeController.java
+++ backend/src/main/java/com/xiaoRed/controller/AuthorizeController.java
@@ -5,6 +5,9 @@ import com.xiaoRed.service.AccountService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/auth")
+@Validated
 public class AuthorizeController {
 
     @Resource
@@ -22,13 +26,13 @@ public class AuthorizeController {
 
     /**
      * 请求邮件验证码
-     * @param type 类型
-     * @param email 请求邮件
+     * @param type 类型， 必须是register或reset中的一个
+     * @param email 请求邮件，必须是合法的邮箱地址才能通过校验
      * @param request 请求，用来获得请求的ip地址
      * @return 是否请求成功
      */
     @GetMapping("/ask-code")
-    public RestBean<Void> askVerifyCode(@RequestParam String type, @RequestParam String email,
+    public RestBean<Void> askVerifyCode(@RequestParam @Pattern(regexp = "(register|reset)") String type, @RequestParam @Email String email,
                                         HttpServletRequest request){
         String message = accountService.sendEmailVerifyCode(type, email, request.getRemoteAddr());
         return message == null ? RestBean.success() : RestBean.failure(400, message);
