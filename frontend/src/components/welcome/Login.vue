622f3e9fe200cf1031d21233a344362f60d28f26
XiaoRedAh <2976698583@qq.com>
2023-08-16 13:24:17 +0800

    封装axios请求

--- frontend/src/components/welcome/Login.vue
+++ frontend/src/components/welcome/Login.vue
@@ -1,12 +1,32 @@
 <script setup>
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import {User,Lock} from '@element-plus/icons-vue';
+import {login} from "@/net/index.js";
+
+const formRef = ref()
 
 const form = reactive({
   username: '',
   password: '',
   remember: false
 })
+
+const rule = {
+  username: [
+    {required: true, message: '请输入用户名'}
+  ],
+  password: [
+    {required: true, message: '请输入密码'}
+  ]
+}
+
+function userLogin(){
+  formRef.value.validate((valid)=>{
+    if(valid){
+      login(form.username, form.password, form.remember, ()=>{})
+    }
+  })
+}
 
 </script>
 
@@ -17,18 +37,24 @@ const form = reactive({
       <div style="font-size: 25px;font-weight: bold">登录</div>
       <div style="font-size: 14px;color:grey">输入用户名和密码进行登录</div>
     </div>
-    <!--输入框-->
+    <!--表单-->
     <div style="margin-top: 50px">
-      <el-input v-model="form.username" type="text" placeholder="用户名/邮箱">
-        <template #prefix>
-          <el-icon><User /></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.password" type="password" style="margin-top: 20px" placeholder="密码">
-        <template #prefix>
-          <el-icon><Lock /></el-icon>
-        </template>
-      </el-input>
+      <el-form :model="form" :rules="rule" ref="formRef">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" type="text" placeholder="用户名/邮箱">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" style="margin-top: 20px" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+      </el-form>
     </div>
     <!--记住我，忘记密码-->
     <div style="margin-top: 10px">
@@ -43,7 +69,7 @@ const form = reactive({
     </div>
     <!--登录/注册按钮-->
     <div style="margin-top: 40px">
-      <el-button style="width: 270px" type="success" plain>登录</el-button>
+      <el-button @click="userLogin" style="width: 270px" type="success" plain>登录</el-button>
     </div>
     <el-divider>
       <span style="color: grey;font-size: 13px">没有账号</span>
