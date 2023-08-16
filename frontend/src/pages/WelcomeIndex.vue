969bd6ad1dc7aa6bde4b0a87d048da5fca1b771d
XiaoRedAh <2976698583@qq.com>
2023-08-16 10:47:16 +0800

    引入element-plus

---
+++ frontend/src/pages/WelcomeIndex.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="layout">
+    <el-container class="container">
+      <el-aside class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.layout{
+  height: 100vh
+}
+
+.container{
+  height: 100vh
+}
+
+.aside{
+  width: 65%;
+  background-color: red
+}
+
+.main{
+  background-color: dodgerblue;
+  width: 35%
+}
+</style>
